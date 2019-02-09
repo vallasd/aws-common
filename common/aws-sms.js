c874c03f8d52c64868e72d8590a13d46edb9d5a0
David Vallas <david.vallas@michaelkors.com>
2019-02-09 08:33:26 -0500

    added header

---
+++ common/aws-sms.js
@@ -0,0 +1,7 @@
+// aws-push.js (handles push notification for ios and android)
+// aws-common
+//
+// Created by David Vallas. (david_vallas@yahoo.com) (dcvallas@twitter)
+// Copyright © 2019 Fenix Labs.
+//
+// All Rights Reserved.
