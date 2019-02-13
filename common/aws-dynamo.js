d828e6c85bf9cf10c52c4b20474be42f2d4465af
David Vallas <david.vallas@michaelkors.com>
2019-02-13 16:56:05 -0500

    added additional service framework files

---
+++ common/aws-dynamo.js
@@ -0,0 +1,7 @@
+// aws-s3.js (handles POST, PUT, GET, DELETE of dynamo items and tables)
+// aws-common
+//
+// Created by David Vallas. (david_vallas@yahoo.com) (dcvallas@twitter)
+// Copyright © 2019 Fenix Labs.
+//
+// All Rights Reserved.
---
+++ common/aws-s3.js
@@ -0,0 +1,7 @@
+// aws-s3.js (handles PUT, GET, DELETE of s3 files)
+// aws-common
+//
+// Created by David Vallas. (david_vallas@yahoo.com) (dcvallas@twitter)
+// Copyright © 2019 Fenix Labs.
+//
+// All Rights Reserved.
