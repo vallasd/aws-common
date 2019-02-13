bf496bf5f879abfa0dcf14d7add12ed894b875f8
David Vallas <david.vallas@michaelkors.com>
2019-02-13 16:36:51 -0500

    updated aws-export.sh

--- aws-export.sh
+++ aws-export.sh
@@ -10,10 +10,7 @@
 
 #!/bin/bash
 
-# check if we aren't making export files
-
-# create exports directory
-
+# get the inputed options
 while getopts "o:" opt; do
   case $opt in
     o) OPTION=$OPTARG;;
@@ -21,96 +18,91 @@ while getopts "o:" opt; do
   esac
 done
 
-# create exports directory
-
-if [ -d ../exports ]; then
-  if [[ ${OPTION} != 'nozip' ]]; then
-    rm -R ../exports/* 2> /dev/null
+makeExportsAndSamCliDirectories () {
+  # create exports directory
+  if [ -d ../exports ]; then
+    if [[ ${OPTION} != 'nozip' ]]; then
+      rm -R ../exports/* 2> /dev/null
+    fi
+  else
+    mkdir ../exports 2> /dev/null
   fi
-else
-  mkdir ../exports 2> /dev/null
-fi
 
-# create samcli directory and copy yaml
-
-if [ ! -d ../samcli ]; then
-  mkdir ../samcli
-  if [[ ${OPTION} != 'nozip' ]]; then
-    cp ./template.yaml ../samcli/. 2> /dev/null
+  # create samcli directory and copy yaml
+  if [ ! -d ../samcli ]; then
+    mkdir ../samcli
+    if [[ ${OPTION} != 'nozip' ]]; then
+      cp ./template.yaml ../samcli/. 2> /dev/null
+    fi
   fi
-fi
+}
 
-# create variables for current directory and aws directories
+getGlobals () {
+  # create variables for current directory and aws directories
+  COMMONDIR=`pwd`
+  cd ..
+  AWS_DIRECTORIES=(aws-*)
+}
 
-COMMONDIR=`pwd`
-cd ..
-AWS_DIRECTORIES=(aws-*)
+copyFilesToExports () {
+  # copy files for export directory
+  cd exports
+  cp -R ${COMMONDIR}/common . 2> /dev/null
+  cp ${COMMONDIR}/lambda.js . 2> /dev/null
+  cp ${COMMONDIR}/app.js . 2> /dev/null
+}
 
-# copy files for export directory
-cd exports
-cp -R ${COMMONDIR}/common . 2> /dev/null
-cp ${COMMONDIR}/lambda.js . 2> /dev/null
-cp ${COMMONDIR}/app.js . 2> /dev/null
-
-
-
-# iterate AWS_DIRECTORIES and create AWS lambda and server zip files
-for directory in "${AWS_DIRECTORIES[@]}"
-do
-
-  # copy request handler to exports
-  cp ../${directory}/actionHandler.js . 2> /dev/null
-  cp ../${directory}/actionCommon.js . 2> /dev/null
-  cp -R ../${directory}/actions . 2> /dev/null
-  cp -R ../${directory}/documents . 2> /dev/null
-
+createZipLists () {
   # create lambda zip list
   find common > lambda.list
-  find actions >> lambda.list
-  find documents >> lambda.list
+  find actions >> lambda.list 2> /dev/null
+  find documents >> lambda.list 2> /dev/null
   echo "actionHandler.js" >> lambda.list
   echo "actionCommon.js" >> lambda.list
   echo "lambda.js" >> lambda.list
 
   # create server zip list
   find common > server.list
-  find actions >> server.list
-  find documents >> server.list
+  find actions >> server.list 2> /dev/null
+  find documents >> server.list 2> /dev/null
   echo "actionHandler.js" >> server.list
   echo "actionCommon.js" >> server.list
   echo "lambda.js" >> server.list
   echo "app.js" >> server.list
   echo "package.json" >> server.list
+}
 
-  if [[ ${OPTION} != 'nozip' ]]; then
+copyProjectFiles () {
+  # copy request handler to exports
+  cp ../${directory}/actionHandler.js . 2> /dev/null
+  cp ../${directory}/actionCommon.js . 2> /dev/null
+  cp -R ../${directory}/actions . 2> /dev/null
+  cp -R ../${directory}/documents . 2> /dev/null
+}
 
-    # copy lambda common package
-    cp ${COMMONDIR}/aws-lambda-package.json .  2> /dev/null
+mergePackages () {
+  # copy lambda common package
+  cp ${COMMONDIR}/$1 .  2> /dev/null
 
-    # merge two packages if they exist or just make one package
-    if [ -f ../${directory}/package.json ]; then
-      cp ../${directory}/package.json ./local-package.json 2> /dev/null
-      jq -s 'reduce .[] as $d ({}; . *= $d)' $(find . -name "*-package.json") > package.json
-      rm ./local-package.json 2> /dev/null
-    else
-      cp aws-lambda-package.json package.json 2> /dev/null
-    fi
-
-    # install package
-    npm install --only=production
-
-    # add node_modules to lambda list
-    find node_modules >> lambda.list
-
-    # create a zip file for lambda
-    cat lambda.list | zip -@ aws-lambda-${directory}.zip > /dev/null
-
-    # create sam cli directory with node modules
-    rm -R ../samcli/${directory} 2> /dev/null
-    mkdir ../samcli/${directory} 2> /dev/null
-    cp -R ./node_modules ../samcli/${directory}/. 2> /dev/null
+  # merge two packages if they exist or just make one package
+  if [ -f ../${directory}/local-package.json ]; then
+    cp ../${directory}/local-package.json .
+    jq -s 'reduce .[] as $d ({}; . *= $d)' $(find . -name "*-package.json") > package.json
+    rm ./local-package.json 2> /dev/null
+  else
+    cp $1 package.json
   fi
+}
 
+installNPM () {
+  # install package
+  npm install --only=production
+
+  # add node_modules to lambda list
+  find node_modules >> lambda.list
+}
+
+copyFilesToSamCli () {
   # create samcli (if necessary) and copy files
   mkdir ../samcli/${directory} 2> /dev/null
   cp -R ./common ../samcli/${directory}/. 2> /dev/null
@@ -124,50 +116,81 @@ do
   cp ./package.json ../samcli/${directory}/. 2> /dev/null
   cp ./package-lock.json ../samcli/${directory}/. 2> /dev/null
   cp ${COMMONDIR}/aws-config.json ../samcli/${directory}/. 2> /dev/null
+}
 
+resetSamCliProjectDir () {
+  # create sam cli directory with node modules
+  rm -R ../samcli/${directory} 2> /dev/null
+  mkdir ../samcli/${directory} 2> /dev/null
+  cp -R ./node_modules ../samcli/${directory}/. 2> /dev/null
+}
+
+removeLambdaFiles () {
   # remove lambda-package
   rm aws-lambda-package.json 2> /dev/null 2> /dev/null
   rm package-lock.json 2> /dev/null 2> /dev/null
   rm -R node_modules 2> /dev/null 2> /dev/null
+}
 
-  if [[ ${OPTION} != 'nozip' ]]; then
-
-    # copy server-package
-    cp ${COMMONDIR}/aws-server-package.json . 2> /dev/null
-
-    # merge two packages if they exist or just make one package
-    if [ -f ../${directory}/package.json ]; then
-      cp ../${directory}/package.json ./local-package.json 2> /dev/null
-      jq -s 'reduce .[] as $d ({}; . *= $d)' $(find . -name "*-package.json") > package.json
-      rm ./local-package.json 2> /dev/null
-    else
-      cp aws-server-package.json package.json
-    fi
-
-    # create a zip file for server
-    cat server.list | zip -@ aws-server-${directory}.zip > /dev/null
-
-    # remove server-package
-    rm aws-server-package.json 2> /dev/null
-
-  fi
-
+removeProjectFiles () {
   rm -R actions 2> /dev/null
   rm -R documents 2> /dev/null
   rm actionHandler.js 2> /dev/null
   rm actionCommon.js 2> /dev/null
+}
 
-done
+removeExportFiles () {
+  rm -R common 2> /dev/null
+  rm package.json 2> /dev/null
+  rm lambda.js 2> /dev/null
+  rm app.js 2> /dev/null
+  rm lambda.list 2> /dev/null
+  rm server.list 2> /dev/null
+}
 
-# remove extra files
+main () {
 
-rm -R common 2> /dev/null
-rm package.json 2> /dev/null
-rm lambda.js 2> /dev/null
-rm app.js 2> /dev/null
-rm lambda.list 2> /dev/null
-rm server.list 2> /dev/null
+  makeExportsAndSamCliDirectories
+  getGlobals
+  copyFilesToExports
 
-echo "Builds completed"
+  # iterate AWS_DIRECTORIES and create AWS lambda and server zip files
+  for directory in "${AWS_DIRECTORIES[@]}"
+  do
+    copyProjectFiles
+    createZipLists
 
+    if [[ ${OPTION} != 'nozip' ]]; then
+      echo "HERE DUDE"
+      mergePackages aws-lambda-package.json
+      installNPM
+
+      # create a zip file for lambda
+      cat lambda.list | zip -@ aws-lambda-${directory}.zip > /dev/null
+
+      resetSamCliProjectDir
+    fi
+
+    copyFilesToSamCli
+    removeLambdaFiles
+
+    if [[ ${OPTION} != 'nozip' ]]; then
+      mergePackages aws-server-package.json
+
+      # create a zip file for server
+      cat server.list | zip -@ aws-server-${directory}.zip > /dev/null
+
+      # remove server-package
+      rm aws-server-package.json 2> /dev/null
+    fi
+
+    removeProjectFiles
+  done
+
+  removeExportFiles
+
+  echo "builds completed!"
+}
+
+main
 exit 0
